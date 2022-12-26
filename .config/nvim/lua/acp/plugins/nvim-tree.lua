1eb25a54762c807873f0b9be70844b7dd534ceed
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2022-12-26 19:05:00 +0530

    [2022.12.26-19:05:00] [Backup] Auto backing up changes

--- .config/nvim/lua/acp/plugins/nvim-tree.lua
+++ .config/nvim/lua/acp/plugins/nvim-tree.lua
@@ -11,8 +11,16 @@ vim.g.loaded_netrwPlugin = 1
 -- change color for arrows in tree to light blue
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 
+local function cd_dot_cb(node)
+	tree.change_dir(vim.fn.getcwd(-1))
+	if node.name ~= ".." then
+		lib.set_index_and_redraw(node.absolute_path)
+	end
+end
+
 -- configure nvim-tree
 nvimtree.setup({
+
 	-- change folder arrow icons
 	renderer = {
 		icons = {
