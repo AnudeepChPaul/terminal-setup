e04df2711898776c7fb28c592976ceeb386651f0
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2022-12-26 23:56:52 +0530

    indentation changes

--- .config/nvim/lua/acp/plugins/nvim-tree.lua
+++ .config/nvim/lua/acp/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 -- import nvim-tree plugin safely
 local setup, nvimtree = pcall(require, "nvim-tree")
 if not setup then
-	return
+  return
 end
 
 -- recommended settings from nvim-tree documentation
@@ -15,46 +15,46 @@ vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
 local api = require("nvim-tree.api")
 
 local function scope_to_node()
-	api.tree.change_root_to_node()
+  api.tree.change_root_to_node()
 end
 
 local function scope_to_parent()
-	api.tree.change_root_to_parent()
+  api.tree.change_root_to_parent()
 end
 
 -- configure nvim-tree
 nvimtree.setup({
-	view = {
-		mappings = {
-			list = {
-				{ key = ">", action = "scope_to_node", action_cb = scope_to_node },
-				{ key = "<", action = "scope_to_parent", action_cb = scope_to_parent },
-			},
-		},
-	},
-	-- change folder arrow icons
-	renderer = {
-		icons = {
-			glyphs = {
-				folder = {
-					arrow_closed = "", -- arrow when folder is closed
-					arrow_open = "", -- arrow when folder is open
-				},
-			},
-		},
-	},
-	-- disable window_picker for
-	-- explorer to work well with
-	-- window splits
-	actions = {
-		open_file = {
-			window_picker = {
-				enable = false,
-			},
-		},
-		change_dir = {
-			enable = true,
-			global = true,
-		},
-	},
+  view = {
+    mappings = {
+      list = {
+        { key = ">", action = "scope_to_node", action_cb = scope_to_node },
+        { key = "<", action = "scope_to_parent", action_cb = scope_to_parent },
+      },
+    },
+  },
+  -- change folder arrow icons
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = "", -- arrow when folder is closed
+          arrow_open = "", -- arrow when folder is open
+        },
+      },
+    },
+  },
+  -- disable window_picker for
+  -- explorer to work well with
+  -- window splits
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false,
+      },
+    },
+    change_dir = {
+      enable = true,
+      global = true,
+    },
+  },
 })
