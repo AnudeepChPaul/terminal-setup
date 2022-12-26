7a46a72b42cd5a6026c2a9407df29a2fdc050d15
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2022-12-27 01:05:01 +0530

    [2022.12.27-01:05:01] [Backup] Auto backing up changes

--- .config/nvim/lua/acp/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/acp/plugins/lsp/null-ls.lua
@@ -17,7 +17,7 @@ null_ls.setup({
 	sources = {
 		--  to disable file types use
 		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-		--formatting.prettier, -- js/ts formatter
+		formatting.prettier, -- js/ts formatter
 		formatting.eslint,
 		formatting.stylua, -- lua formatter
 		diagnostics.eslint_d.with({ -- js/ts linter
