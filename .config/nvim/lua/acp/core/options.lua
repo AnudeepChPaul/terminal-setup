86a8cd1a4179fc001e16b33632553f0b39a5dedb
Anudeep Chandra Paul <anudeepchandrapaul@Anudeeps-MacBook-Pro.local>
2022-12-27 18:59:58 +0530

    [2022.12.27-18:59:58] [Backup] Auto backing up changes

--- .config/nvim/lua/acp/core/options.lua
+++ .config/nvim/lua/acp/core/options.lua
@@ -1,19 +1,36 @@
+vim.cmd("autocmd!")
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.title = true
+vim.opt.backup = false
+vim.opt.laststatus = 2
+vim.opt.scrolloff = 10
+
 -- Line Numbers
 vim.opt.relativenumber = false
 vim.opt.number = true
+vim.wo.number = true
 
 -- Tabs & Indentation
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-
--- Line Wrapping
 vim.opt.wrap = false
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.smartindent = true
+
+-- Backspace
+vim.opt.backspace = "indent,eol,start"
 
 -- Search Settings
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.hlsearch = true
 
 -- Cursor Settings
 vim.opt.cursorline = true
@@ -22,9 +39,8 @@ vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.opt.signcolumn = "yes"
-
--- Backspace
-vim.opt.backspace = "indent,eol,start"
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
 
 -- Clipboard
 vim.opt.clipboard:append("unnamedplus")
@@ -33,4 +49,9 @@ vim.opt.clipboard:append("unnamedplus")
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+-- While searching search within sub dirs
+vim.opt.path:append({ "**" })
 vim.opt.iskeyword:append("-")
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append({ "r" })
