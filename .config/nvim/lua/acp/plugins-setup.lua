29a46cf74d35e5ac69c19fa6a1b6a6e6a4a53408
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2022-12-27 20:43:30 +0530

    Update plugins-setup.lua

--- .config/nvim/lua/acp/plugins-setup.lua
+++ .config/nvim/lua/acp/plugins-setup.lua
@@ -1,6 +1,6 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-	local fn = vim.fn
+  local fn = vim.fn
 	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 	if fn.empty(fn.glob(install_path)) > 0 then
 		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
