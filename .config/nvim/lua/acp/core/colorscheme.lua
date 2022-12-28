4bfa6c2f726f308c34e5fee43f570abe9088cafa
Anudeep Chandra Paul <anudeepchandrapaul@Anudeeps-MacBook-Pro.local>
2022-12-29 01:30:00 +0530

    [2022.12.29-01:30:00] [Backup] Auto backing up changes

--- .config/nvim/lua/acp/core/colorscheme.lua
+++ .config/nvim/lua/acp/core/colorscheme.lua
@@ -5,3 +5,12 @@ if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
 end
+
+local color_status, colorizer = pcall(require, "colorizer")
+if not color_status then
+	return
+end
+
+colorizer.setup({
+	"*",
+})
