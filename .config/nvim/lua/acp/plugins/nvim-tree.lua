e54d8a79af2c0f99c6950a4b1799a634dd0f69a6
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2022-12-30 02:40:00 +0530

    [2022.12.30-02:40:00] [Backup] Auto backing up changes

--- .config/nvim/lua/acp/plugins/nvim-tree.lua
+++ .config/nvim/lua/acp/plugins/nvim-tree.lua
@@ -20,7 +20,7 @@ local function scope_to_parent()
 end
 
 local function toggle_ignore()
-  api.tree.toggle_gitignore_filter()
+  api.tree.toggle_hidden_filter()
 end
 
 -- configure nvim-tree
@@ -30,7 +30,7 @@ nvimtree.setup({
       list = {
         { key = ">", action = "scope_to_node", action_cb = scope_to_node },
         { key = "<", action = "scope_to_parent", action_cb = scope_to_parent },
-        { key = "!", action = "toggle_ignore", action_cb = scope_to_parent },
+        { key = "<leader>h", action = "toggle_ignore", action_cb = scope_to_parent },
       },
     },
   },
