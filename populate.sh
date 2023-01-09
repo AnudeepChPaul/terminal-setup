0ad1253ce03e82cfbe8482c88e7881ac45204c57
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2023-01-09 09:35:32 +0530

    Update populate.sh

--- populate.sh
+++ populate.sh
@@ -6,10 +6,13 @@ cp -f -r ./.config/tmux/* ~/.config/tmux
 cp -f -r .zprofile ~
 cp -f -r .zshrc ~
 
-cp -aR .config/fish/config.fish ~/.config/fish/
-cp -aR .config/fish/functions/nvm.fish ~/.config/fish/functions/
-cp -aR .config/fish/functions/load_nvm.fish ~/.config/fish/functions/
-cp -aR .config/fish/functions/nvm_find_nvmrc.fish ~/.config/fish/functions/
-# cp -aR .config/fish/functions/fish_user_key_bindings.fish ~/.config/fish/functions/
+cp -aR .config/custom_scripts                                 ~/.config/custom_scripts
+
+cp -aR .config/fish/env.fish                               ~/.config/fish/
+cp -aR .config/fish/config.fish                               ~/.config/fish/
+cp -aR .config/fish/functions/nvm.fish                        ~/.config/fish/functions/
+cp -aR .config/fish/functions/load_nvm.fish                   ~/.config/fish/functions/
+cp -aR .config/fish/functions/nvm_find_nvmrc.fish             ~/.config/fish/functions/
+# cp -aR .config/fish/functions/fish_user_key_bindings.fish   ~/.config/fish/functions/
 
 
