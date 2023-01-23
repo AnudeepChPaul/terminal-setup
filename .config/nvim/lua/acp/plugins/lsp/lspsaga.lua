3431bb6689888111c3ef7499ce95a0c21a90fd84
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2023-01-23 14:12:42 +0530

    Update lspsaga.lua

--- .config/nvim/lua/acp/plugins/lsp/lspsaga.lua
+++ .config/nvim/lua/acp/plugins/lsp/lspsaga.lua
@@ -1,27 +1,32 @@
 -- import lspsaga safely
 local saga_status, saga = pcall(require, "lspsaga")
 
-saga.init_lsp_saga({
-  server_filetype_map = {
-    typescript = "typescript",
-  },
+-- saga.init_lsp_saga({
+--   server_filetype_map = {
+--     typescript = "typescript",
+--   },
+--   -- keybinds for navigation in lspsaga window
+--   move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+--   -- use enter to open file with finder
+--   finder_action_keys = {
+--     open = "<CR>",
+--   },
+--   -- use enter to open file with definition preview
+--   definition_action_keys = {
+--     edit = "<CR>",
+--   },
+-- })
+
+saga.setup({
   -- keybinds for navigation in lspsaga window
-  move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-  -- use enter to open file with finder
-  finder_action_keys = {
-    open = "<CR>",
-  },
+  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
   -- use enter to open file with definition preview
-  definition_action_keys = {
+  definition = {
     edit = "<CR>",
   },
+  ui = {
+    -- colors = {
+    --   normal_bg = "#022746",
+    -- },
+  },
 })
-
--- LSP Saga Keymaps
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
-vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
-vim.keymap.set("i", "<C-k>", "<Cmd>Lspsaga signature_help<CR>", opts)
-vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
-vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
