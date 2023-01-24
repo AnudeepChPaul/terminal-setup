77bd559203db1fafccb34a40132ea0ed249d2923
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2023-01-24 19:16:25 +0530

    Update keymaps.lua

--- .config/nvim/lua/acp/core/keymaps.lua
+++ .config/nvim/lua/acp/core/keymaps.lua
@@ -115,14 +115,27 @@ Map("n", "<leader>rr", ":NvimTreeRefresh<CR>") -- find files within current work
 Map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 Map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 Map("n", "K", "<Cmd>Lspsaga hover_doc ++quiet ++keep<CR>", opts)
-Map("n", "<leader>gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
 Map("i", "<leader>k", "<Cmd>Lspsaga signature_help<CR>", opts)
-Map("n", "<leader>gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
 Map("n", "<leader>rn", "<Cmd>Lspsaga rename<CR>", opts)
 Map("n", "<leader>rnn", "<cmd>Lspsaga rename ++project<CR>", opts)
-Map({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+Map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
 Map("n", "<leader>ci", "<cmd>Lspsaga incoming_calls<CR>", opts)
 Map("n", "<leader>co", "<cmd>Lspsaga outgoing_calls<CR>", opts)
+Map("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+Map("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+Map("n", "<leader>ot", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+Map("n", "<leader>f", format(bufnr), opts)
+Map("n", "<leader>gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
+Map("n", "<leader>gf", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+Map("n", "<leader>gi", vim.lsp.buf.implementation, opts)
+Map("n", "<leader>gr", vim.lsp.buf.references, opts)
+Map("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+Map("n", "<leader>gd", vim.lsp.buf.definition, opts)
+
+-- typescript specific keymaps (e.g. rename file and update imports)
+Map("n", "rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
+Map("n", "oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
+Map("n", "ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
 
 -- telescope
 Map("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
@@ -132,7 +145,6 @@ Map("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live
 Map("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 Map("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 Map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
-
 Map("n", "<leader>gg", "<cmd>LazyGit<CR>")
 
 -- telescope git commands (not on youtube nvim video)
