05870448f0cc70be67b2a20988f92e56330abe84
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2023-03-01 01:39:17 +0530

    Delete .config directory

--- .config/nvim/lua/acp/plugins/comment.lua
+++
@@ -1,5 +0,0 @@
--- import comment plugin safely
-local setup, comment = pcall(require, "Comment")
-
--- enable comment
-comment.setup()
--- .config/nvim/lua/acp/plugins/gitsigns.lua
+++
@@ -1,5 +0,0 @@
--- import gitsigns plugin safely
-local setup, gitsigns = pcall(require, "gitsigns")
-
--- configure/enable gitsigns
-gitsigns.setup()
