95fd2f6493dde8b91e64317f8875bc533a822831
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2023-04-06 15:54:57 +0530

    Update setup.zsh.sh

--- setup/setup.zsh.sh
+++ setup/setup.zsh.sh
@@ -5,19 +5,15 @@
 zsh --version
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/AnudeepChPaul/terminal-setup.git ${HOME}/terminal-setup
-
-cp -r ${HOME}/terminal-setup/.config/* ${HOME}/.config
-cp ${HOME}/terminal-setup/.config/tmux/tmux.conf ${HOME}/.config/tmux.conf
-rm -rf ${HOME}/.config/nvim/plugin/packer_compiled.lua
-cp ${HOME}/terminal-setup/.zshrc ${HOME}/.zshrc
-cp ${HOME}/terminal-setup/.zprofile ${HOME}/.zprofile
-
-nvim .
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $HOME/.oh-my-zsh/custom/plugins/zsh-autocomplete
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/kazhala/dotbare.git $HOME/.oh-my-zsh/custom/plugins/dotbare
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+git clone https://github.com/wfxr/forgit.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/forgit
+git clone git@github.com:grigorii-zander/zsh-npm-scripts-autocomplete.git ~/.oh-my-zsh/custom/plugins/zsh-npm-scripts-autocomplete
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 
