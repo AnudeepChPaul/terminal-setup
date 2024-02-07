dd52e7fd04c551e9bfbc2a3318b2f8f7c139a27f
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2024-02-07 23:12:22 +0530

    Adding brewfile

--- setup/setup.macos.sh
+++ setup/setup.macos.sh
@@ -6,23 +6,21 @@ echo "==============Installing curl=============="
 brew install curl
 
 echo "==============Installing git=============="
-brew install git gh lazygit
-
 gh auth login
 
-echo "==============Setting up shell utils=============="
-brew install fzf tree bat zoxide
-brew install tmux neovim ripgrep
-brew install lua-language-server fd exa
-brew install alacritty starship ranger mdcat
+echo "==============Setting up dotfiles=============="
+git clone https://github.com/kazhala/dotbare.git ~/.dotbare
+export DOTBARE_DIR="$HOME/.cfg"
+export DOTBARE_TREE="$HOME"
+dotbare finit -u git@github.com:AnudeepChPaul/terminal-setup.git
 
+echo "==============Installing Brewfile=============="
+brew bundle install
+
+echo "==============Setting up shell utils=============="
 . common.utils.sh
 
 ./setup.zsh.sh
-
-echo "==============Installing universal ctags=============="
-brew tap universal-ctags/universal-ctags
-brew install --HEAD universal-ctags
 
 echo "==============Starting nvm=============="
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -33,5 +31,3 @@ mkdir $HOME/bin
 echo "==============Downloading JDK=============="
 curl -o $HOME/bin/jdk.default.tar.gz https://download.java.net/java/GA/jdk18.0.2/f6ad4b4450fd4d298113270ec84f30ee/9/GPL/openjdk-18.0.2_macos-aarch64_bin.tar.gz
 tar -xvf jdk.default.tar.gz
-
-brew install scc rm-improved ddgr ripgrep tldr
