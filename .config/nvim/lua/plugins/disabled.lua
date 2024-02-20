b4f2ea3bce828c0171d441b53fe21494be2612ec
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2024-02-20 13:48:39 +0530

    Nvim updates

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "echasnovski/mini.surround", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.bufremove", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
 }
